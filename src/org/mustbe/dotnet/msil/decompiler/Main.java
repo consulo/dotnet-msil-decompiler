2af09fdb46d27ebb7254811e2922e31aa4c6ba2d
VISTALL <vistall.valeriy@gmail.com>
2015-05-11 02:05:03 +0300

    initial code

---
+++ src/org/mustbe/dotnet/msil/decompiler/Main.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2013-2015 must-be.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mustbe.dotnet.msil.decompiler;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import org.mustbe.dotnet.msil.decompiler.file.DotNetArchiveFile;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.vfs.ArchiveEntry;
+import edu.arizona.cs.mbel.mbel.ModuleParser;
+
+/**
+ * @author VISTALL
+ * @since 11.05.2015
+ */
+public class Main
+{
+	public static void main(String[] args) throws Exception
+	{
+		if(args.length != 1)
+		{
+			System.out.println("Use '<target-file>'");
+		}
+
+		File file = new File(args[0]);
+		ModuleParser moduleParser = new ModuleParser(file);
+		moduleParser.parseNext();
+
+		DotNetArchiveFile archiveFile = new DotNetArchiveFile(file, moduleParser, System.currentTimeMillis());
+
+		File outFile = new File(file.getParentFile(), FileUtil.getNameWithoutExtension(file.getName()) + ".zip");
+
+
+		FileOutputStream fileOutputStream = new FileOutputStream(outFile);
+		ZipOutputStream zipOutputStream = new ZipOutputStream(fileOutputStream);
+		Iterator<? extends ArchiveEntry> entries = archiveFile.entries();
+		while(entries.hasNext())
+		{
+			ArchiveEntry next = entries.next();
+
+			ZipEntry zipEntry = new ZipEntry(next.getName());
+			zipOutputStream.putNextEntry(zipEntry);
+
+			InputStream inputStream = archiveFile.getInputStream(next);
+
+			byte[] bytes = new byte[1024];
+			int length;
+			while((length = inputStream.read(bytes)) >= 0)
+			{
+				zipOutputStream.write(bytes, 0, length);
+			}
+
+			zipOutputStream.closeEntry();
+			inputStream.close();
+		}
+
+		zipOutputStream.close();
+		fileOutputStream.close();
+	}
+}
---
+++ src/org/mustbe/dotnet/msil/decompiler/util/MsilHelper.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2013-2014 must-be.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mustbe.dotnet.msil.decompiler.util;
+
+import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.util.text.StringUtil;
+
+/**
+ * @author VISTALL
+ * @since 22.05.14
+ */
+public class MsilHelper
+{
+	public static final char GENERIC_MARKER_IN_NAME = '`';
+	public static final char NESTED_SEPARATOR_IN_NAME = '/';
+	public static final char NORMAL_SEPARATOR_IN_NAME = '.';
+
+	public static final String CONSTRUCTOR_NAME = ".ctor";
+	public static final String STATIC_CONSTRUCTOR_NAME = ".cctor";
+
+	@NotNull
+	public static String prepareForUser(@NotNull String name)
+	{
+		String newName = cutGenericMarker(name);
+		newName = newName.replace(NESTED_SEPARATOR_IN_NAME, NORMAL_SEPARATOR_IN_NAME);
+		return newName;
+	}
+
+	@NotNull
+	public static String cutGenericMarker(@NotNull String name)
+	{
+		int nested = name.lastIndexOf(NESTED_SEPARATOR_IN_NAME);
+		int i = name.lastIndexOf(GENERIC_MARKER_IN_NAME);
+		if(i > 0)
+		{
+			// if we dont have nested like System.Collection.Generic.List`1
+			if(nested == -1)
+			{
+				name = name.substring(0, i);
+			}
+			// if we have nested like System.Collection.Generic.List`1/Enumerator
+			else
+			{
+				StringBuilder builder = new StringBuilder(name.length() - 2);
+				char[] chars = name.toCharArray();
+
+				boolean genericEntered = false;
+				for(char aChar : chars)
+				{
+					switch(aChar)
+					{
+						case '`':
+							genericEntered = true;
+							break;
+						case '/':
+							aChar = '.';
+						default:
+							if(genericEntered)
+							{
+								if(Character.isDigit(aChar))
+								{
+									continue;
+								}
+								else
+								{
+									genericEntered = false;
+								}
+							}
+							builder.append(aChar);
+							break;
+					}
+				}
+				name = builder.toString();
+			}
+		}
+		return name;
+	}
+
+	public static String appendNoGeneric(String namespace, String name)
+	{
+		if(StringUtil.isEmpty(namespace))
+		{
+			return cutGenericMarker(name);
+		}
+		else
+		{
+			return namespace + "." + cutGenericMarker(name);
+		}
+	}
+
+	public static String append(String namespace, String name)
+	{
+		if(StringUtil.isEmpty(namespace))
+		{
+			return name;
+		}
+		else
+		{
+			return namespace + "." + name;
+		}
+	}
+}
